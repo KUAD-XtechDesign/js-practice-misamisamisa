9b33cf1f209b8d6972e76a97d37fe29e23c91889
misamisamisa <m-nakamori7278@st.kyoto-art.ac.jp>
2020-10-16 15:53:14 +0900

    js9

---
+++ sample/js/script02.js
@@ -0,0 +1,18 @@
+$(function(){
+
+ let txt1 = "おはようございます！"；
+ let txt2 ="お元気ですか？"；
+ let num1 = 20;
+ let num2 = 32;
+
+ $("#button01").on("click",function(){
+     $("#content").text("こんにちは！")；
+ }
+
+ $("#button02").on("click",function(){
+     $("#content").text(txt1);
+ })
+
+ $("#button03").on("click",function(){
+    $("#content").text(txt1+txt2);
+})
