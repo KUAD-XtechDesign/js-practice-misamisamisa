6a61668ee8b7b620caa1967b737cb1180875ab7e
misamisamisa <m-nakamori7278@st.kyoto-art.ac.jp>
2020-10-16 15:58:44 +0900

    js10

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,17 +1,17 @@
 $(function(){
 
- let txt1 = "おはようございます！"；
- let txt2 ="お元気ですか？"；
+ let txt1 = "おはようございます！";
+ let txt2 ="お元気ですか？";
  let num1 = 20;
  let num2 = 32;
 
  $("#button01").on("click",function(){
-     $("#content").text("こんにちは！")；
+     $("#content").text("こんにちは！");
  }
 
  $("#button02").on("click",function(){
-     $("#content").text(txt1);
- })
+    $("#content").text(txt1);
+})
 
  $("#button03").on("click",function(){
     $("#content").text(txt1+txt2);
